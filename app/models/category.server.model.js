542b9fcbdfcf05ccbba21b7b5995332745e363a5
bstjohn <bstjohn@pdx.edu>
2015-07-05 09:56:55 -0700

    Created category model for saving category types (books, movies, etc.)

--- app/models/category.server.model.js
+++ app/models/category.server.model.js
@@ -7,11 +7,37 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 /**
+ * Validation function
+ */
+function validateLength (v) {
+	// A custom validation function for checking string length to be used by the model
+	return v.length <= 15;
+}
+
+/**
  * Category Schema
  */
 var CategorySchema = new Schema({
-	// Category model fields   
-	// ...
+	// The property name
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	description: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	name: {
+		type: String,
+		default: '',
+		trim: true,
+		unique: true,
+		// TODO: Place these into CONSTANTS file.
+		required: 'name cannot be blank',
+		validate: [validateLength, 'name must be 15 characters or less in length']
+	}
 });
 
+// Exposes the model to other objects (kind of like a 'public' setter)
 mongoose.model('Category', CategorySchema);
--- app/tests/category.server.model.test.js
+++ app/tests/category.server.model.test.js
@@ -5,7 +5,7 @@
  */
 var should = require('should'),
 	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
+	//User = mongoose.model('User'),
 	Category = mongoose.model('Category');
 
 /**
@@ -17,38 +17,88 @@ var user, category;
  * Unit tests
  */
 describe('Category Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
-
-		user.save(function() { 
-			category = new Category({
-				// Add model fields
-				// ...
-			});
-
-			done();
-		});
-	});
+	//beforeEach(function(done) {
+	//	user = new User({
+	//		firstName: 'Full',
+	//		lastName: 'Name',
+	//		displayName: 'Full Name',
+	//		email: 'test@test.com',
+	//		username: 'username',
+	//		password: 'password'
+	//	});
+    //
+	//	user.save(function() {
+	//		category = new Category({
+	//			// Add model fields
+	//			// ...
+	//		});
+    //
+	//		done();
+	//	});
+	//});
 
 	describe('Method Save', function() {
 		it('should be able to save without problems', function(done) {
-			return category.save(function(err) {
+			var category = new Category({
+				name: 'Books',
+				description: 'A description of books'
+			});
+
+			category.save(function(err, saved) {
 				should.not.exist(err);
 				done();
+			});
+		});
+
+		it('should throw validation error when name is empty', function(done) {
+			var category = new Category({
+				description: 'Oops! Forgot to add a name.'
+			});
+
+			category.save(function(err) {
+				should.exist(err);
+				err.errors.name.message.should.equal('name cannot be blank');
+				done();
+			});
+		});
+
+		it('should throw validation error when name longer than 15 characters', function(done) {
+			var category = new Category({
+				name: 'Long name! 123456789101112131415'
+			});
+
+			category.save(function(err) {
+				should.exist(err);
+				err.errors.name.message.should.equal('name must be 15 characters or less in length');
+				done();
+			});
+		});
+
+		it('should throw validation error for a duplicate category name', function(done) {
+			var category = new Category({
+				name: 'Books'
+			});
+
+			category.save(function(err) {
+				should.not.exist(err);
+
+				var duplicate = new Category({
+					name: 'Books'
+				});
+
+				duplicate.save(function(err) {
+					err.err.indexOf('$name').should.not.equal(-1);
+					err.err.indexOf('duplicate key error').should.not.equal(-1);
+					should.exist(err);
+					done();
+				});
 			});
 		});
 	});
 
 	afterEach(function(done) { 
 		Category.remove().exec();
-		User.remove().exec();
+		//User.remove().exec();
 
 		done();
 	});
