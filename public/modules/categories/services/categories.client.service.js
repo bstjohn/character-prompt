0022a8f196aa9a817221dfc8877f76945050cd86
bstjohn <bstjohn@pdx.edu>
2015-07-05 17:34:04 -0700

    Got category functionality working.

--- public/modules/categories/services/categories.client.service.js
+++ public/modules/categories/services/categories.client.service.js
@@ -1,6 +1,8 @@
 'use strict';
 
-//Categories service used to communicate Categories REST endpoints
+/**
+ * Categories service used to communicate Categories REST endpoints
+ */
 angular.module('categories').factory('Categories', ['$resource',
 	function($resource) {
 		return $resource('categories/:categoryId', { categoryId: '@_id'
