a888266a29cb6c76809e34b814b1982687a6faa5
bstjohn <bstjohn@pdx.edu>
2015-07-12 16:12:01 -0700

    Applying filters to book output

---
+++ public/modules/books/filters/capitalize.client.filter.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('books').filter('capitalize', [
+	function() {
+		return function(input, allWords) {
+			if (angular.isUndefined(input) || input === null) {
+                return '';
+			}
+
+            if (allWords) {
+                return input.replace(
+                    /([^\W_]+[^\s-]*) */g,
+                    function(txt) {
+                        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                    }
+                );
+            }
+
+            return input.charAt(0).toUpperCase() + input.substr(1).toLowerCase();
+		};
+	}
+]);
