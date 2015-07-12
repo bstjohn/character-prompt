fa5834cb52ea3da545d15e1581018d83079b14af
bstjohn <bstjohn@pdx.edu>
2015-07-12 12:16:07 -0700

    Almost done with Books

--- public/modules/books/config/books.client.routes.js
+++ public/modules/books/config/books.client.routes.js
@@ -1,13 +1,27 @@
 'use strict';
 
-//Setting up route
+/**
+ * The books route
+ */
 angular.module('books').config(['$stateProvider',
-	function($stateProvider) {
-		// Books state routing
-		$stateProvider.
-		state('books', {
-			url: '/books',
-			templateUrl: 'modules/books/views/books.client.view.html'
-		});
-	}
+    function($stateProvider) {
+        // Books state routing
+        $stateProvider.
+            state('listBooks', {
+                url: '/books',
+                templateUrl: 'modules/books/views/books.client.view.html'
+            }).
+            state('createBook', {
+                url: '/books/create',
+                templateUrl: 'modules/books/views/create-book.client.view.html'
+            }).
+            state('viewBook', {
+                url: '/books/:bookId',
+                templateUrl: 'modules/books/views/view-book.client.view.html'
+            }).
+            state('editBook', {
+                url: '/books/:bookId/edit',
+                templateUrl: 'modules/books/views/edit-book.client.view.html'
+            });
+    }
 ]);
